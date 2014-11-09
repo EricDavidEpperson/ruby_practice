5839d564c791fba6d56c26564e64f0976e5c4152
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-11-09 17:23:45 -0600

    bowling_score passes first test

--- bowling_score.rb
+++ bowling_score.rb
@@ -1,2 +1,12 @@
+require 'pry'
+
 class BowlingScore
+
+  def initialize(score)
+    @rolls = score
+  end
+
+  def game_score
+    score = @rolls
+  end
 end
--- bowling_test.rb
+++ bowling_test.rb
@@ -2,4 +2,9 @@ require 'minitest/autorun'
 require_relative 'bowling_score'
 
 class BowlingScoreTest < MiniTest::Unit::TestCase
+
+  def test_score_equals_roll
+    score = BowlingScore.new(10)
+    assert_equal 10, score.game_score
+  end
 end
